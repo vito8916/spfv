1dc086fae01672e88f1e7a45db35590fe7cfe45f
Victor Alvarado <vito8916@gmail.com>
2025-03-24 13:01:27 -0600

    feat: Add extra button size variant and optimize logo loading with priority attribute

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,6 +5,7 @@ const Logo = () => {
     return (
         <div className="h-8 w-8 lg:w-auto lg:h-auto rounded-lg flex items-center justify-center">
             <Image
+                priority
                 src="/spfv-short-logo.svg"
                 width={44}
                 height={44}
@@ -12,6 +13,7 @@ const Logo = () => {
                 className="h-8 w-8 block md:hidden "
             />
             <Image
+                priority
                 src="/spfv-long-logo.svg"
                 width={44}
                 height={44}
