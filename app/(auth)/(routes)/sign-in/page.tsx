3068953cd965f28a6edd66df828c4ea6247dd450
Victor Alvarado <vito8916@gmail.com>
2025-03-24 13:01:15 -0600

    feat: Add metadata for Sign In and Sign Up pages to improve SEO and user context

--- app/(auth)/(routes)/sign-in/page.tsx
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import LoginForm from "@/components/auth/login-form";
+
+export const metadata: Metadata = {
+    title: "Sign In | Sp Fair Value",
+    description: "Sign in to your account",
+};
 
 const SignInPage = () => {
     return (
--- app/(auth)/(routes)/sign-up/page.tsx
+++ app/(auth)/(routes)/sign-up/page.tsx
@@ -1,5 +1,10 @@
 import RegisterForm from "@/components/auth/register-form";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Sign Up | Sp Fair Value",
+    description: "Sign up for an account",
+};
 
 const SignUpPage = () => {
     return (
