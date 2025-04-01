a9a548508ccfe8bb00adae56d3fe11991ef3afb6
Victor Alvarado <vito8916@gmail.com>
2025-04-01 16:01:18 -0600

    feat: Add SpfvIconTool component with SVG icon for the SPFV tool

---
+++ components/shared/spfv-icon-tool.tsx
@@ -0,0 +1,62 @@
+export default function SpfvIconTool() {
+  return (
+    <svg
+      className="w-5 h-5 text-primary"
+      width="52"
+      height="53"
+      viewBox="0 0 52 53"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <mask id="path-1-inside-1_298_409" fill="white">
+        <rect width="24" height="24" rx="3" />
+      </mask>
+      <rect
+        width="24"
+        height="24"
+        rx="3"
+        fill="#FF5722"
+        stroke="#FF5722"
+        strokeWidth="12"
+        mask="url(#path-1-inside-1_298_409)"
+      />
+      <rect x="28" y="28" width="24" height="24" rx="3" fill="#FF5722" />
+      <path
+        d="M31 12L49 12"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M3 41L21 41"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M31 3L49 3"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M3 32L21 32"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M31 21L49 21"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M3 50L21 50"
+        stroke="#222425"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
