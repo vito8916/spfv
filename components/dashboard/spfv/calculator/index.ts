6ab21966245248418a77edb491d4ce9ef5bcab1a
Victor Alvarado <vito8916@gmail.com>
2025-04-10 21:53:03 -0600

    feat: Add index file to export MarketClosedMessage, TiersSection, LoadingChain, ErrorMessage, and ResultsSection components for improved modularity in the options calculator

---
+++ components/dashboard/spfv/calculator/index.ts
@@ -0,0 +1,5 @@
+export { default as MarketClosedMessage } from './MarketClosedMessage';
+export { default as TiersSection } from './TiersSection';
+export { default as LoadingChain } from './LoadingChain';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as ResultsSection } from './ResultsSection'; 
