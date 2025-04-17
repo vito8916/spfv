5bd334fc99896610e438a67273c3c563e962883c
Victor Alvarado <vito8916@gmail.com>
2025-04-17 13:28:38 -0600

    feat: Add OptionTypeSelectorRadio component for selecting option types with visual indicators

---
+++ components/dashboard/spfv/options/option-type-selector-radio.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import React, { useState } from 'react'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+import { Skeleton } from '@/components/ui/skeleton'
+import { BarChart2, LineChart } from 'lucide-react'
+
+type OptionType = 'chain' | 'atr'
+
+export function OptionTypeSelectorRadio({ onOptionTypeSelect }: { onOptionTypeSelect: (optionType: string) => void }) {
+  const [selectedOption, setSelectedOption] = useState<OptionType>('chain')
+
+  return (
+    <div className="w-full">
+      <div className="flex rounded-md p-1 bg-slate-100 dark:bg-slate-800 gap-x-2">
+        <RadioOption 
+          id="option-chain"
+          value="chain"
+          selected={selectedOption === 'chain'}
+          onChange={() => {
+            setSelectedOption('chain')
+            onOptionTypeSelect('chain')
+          }}
+          icon={<LineChart className="mr-2 h-4 w-4" />}
+          label="Option Chain"
+        />
+        <RadioOption 
+          id="option-atr"
+          value="atr"
+          selected={selectedOption === 'atr'}
+          onChange={() => {
+            setSelectedOption('atr')
+            onOptionTypeSelect('atr')
+          }}
+          icon={<BarChart2 className="mr-2 h-4 w-4" />}
+          label="ATR"
+        />
+      </div>
+    </div>
+  )
+}
+
+interface RadioOptionProps {
+  id: string
+  value: string
+  selected: boolean
+  onChange: () => void
+  icon?: React.ReactNode
+  label: string
+}
+
+function RadioOption({ id, value, selected, onChange, icon, label }: RadioOptionProps) {
+  return (
+    <div className="">
+      <input
+        type="radio"
+        id={id}
+        value={value}
+        checked={selected}
+        onChange={onChange}
+        className="sr-only"
+      />
+      <Label
+        htmlFor={id}
+        className={cn(
+          "flex items-center justify-center rounded-md px-3 py-2 text-sm font-medium cursor-pointer transition-all",
+          selected
+            ? "bg-white dark:bg-slate-950 text-primary shadow-sm"
+            : "hover:bg-slate-200 dark:hover:bg-slate-700 text-muted-foreground"
+        )}
+      >
+        {icon}
+        {label}
+      </Label>
+    </div>
+  )
+}
+
+export function OptionTypeSelectorSkeleton() {
+  return (
+    <div className="flex w-full gap-2">
+      <Skeleton className="h-10 w-full rounded-md" />
+    </div>
+  )
+} 
