d3c23a5db0d7c0a29b8c096845d304345779e82f
Victor Alvarado <vito8916@gmail.com>
2025-04-17 13:29:39 -0600

    refactor: Update useSpfvTop hook to change API endpoint and remove unnecessary console logs

--- hooks/useSpfvTop.ts
+++ hooks/useSpfvTop.ts
@@ -3,24 +3,17 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function useSpfvTop(type: string, intervalTime = 0) {
-  console.log("type:::::::", type);
-  console.log("intervalTime::::::", intervalTime);
-
   const shouldFetch = type;
-  console.log("shouldFetch::::::", shouldFetch);
 
   const { data, error, isLoading, mutate } = useSWR(
-    shouldFetch ? `/api/spfv/spfv-top?type=${type}` : null,
+    shouldFetch ? `/api/spfv/get-spfv-top?type=${type}` : null,
     fetcher,
     {
       refreshInterval: intervalTime,
       revalidateOnFocus: false,
-      //dedupingInterval: 5000,
+      shouldRetryOnError: false,
     }
   );
-
-  console.log("data::::::", data);
-  
   return {
     data: data?.items,
     error,
