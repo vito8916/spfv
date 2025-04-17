f65b750437817be9b034123574ee254c3dfbe65e
Victor Alvarado <vito8916@gmail.com>
2025-04-17 14:31:46 -0600

    refactor: Update bank holiday handling in isMarketOpen function and simplify date initialization

--- utils/utils.ts
+++ utils/utils.ts
@@ -50,6 +50,8 @@ export function isMarketOpen(): boolean {
   // Verifica si es sábado o domingo
   if (day === 0 || day === 6) return false;
 
+  // Check if it's a bank holiday
+  //if (bankHoliday(nyTime)) return false;
 
   // Verifica si está dentro del horario de mercado (9:30am - 4:00pm ET)
   if (
@@ -59,48 +61,35 @@ export function isMarketOpen(): boolean {
     return true;
   }
 
-  if (day === 4) {
+  if(day === 4) {
     return true;
   }
 
   return false;
 }
 
-//get the fridays that are bank holidays in the US from 2024 to 2028
-/* export function getFridaysThatAreBankHolidays(year: number): Date[] {
-  const fridays = [];
-  for (let i = 0; i < 5; i++) {
-    const date = new Date(year, 0, 1);
-    date.setDate(date.getDate() + i);
-    if (isFriday(date) && !isBankHoliday(date)) {
-      fridays.push(date);
-    }
-  }
-  return fridays;
-} */
-
 const usBankHolidays = [
   // new york time zone
-    new Date('2025-01-01').toLocaleString("en-US", { timeZone: "America/New_York" }), // New Year's Day
-    new Date('2025-01-16').toLocaleString("en-US", { timeZone: "America/New_York" }), // Martin Luther King Jr. Day
-    new Date('2025-02-17').toLocaleString("en-US", { timeZone: "America/New_York" }), // Presidents Day
-    new Date('2025-03-17').toLocaleString("en-US", { timeZone: "America/New_York" }), // St. Patrick's Day
-    new Date('2025-04-18').toLocaleString("en-US", { timeZone: "America/New_York" }), // Good Friday
-    new Date('2025-05-26').toLocaleString("en-US", { timeZone: "America/New_York" }), // Memorial Day
-    new Date('2025-07-04').toLocaleString("en-US", { timeZone: "America/New_York" }), // Independence Day
-    new Date('2025-09-01').toLocaleString("en-US", { timeZone: "America/New_York" }), // Labor Day
-    new Date('2025-10-13').toLocaleString("en-US", { timeZone: "America/New_York" }), // Columbus Day
-    new Date('2025-11-11').toLocaleString("en-US", { timeZone: "America/New_York" }), // Veterans Day
-    new Date('2025-11-27').toLocaleString("en-US", { timeZone: "America/New_York" }), // Thanksgiving Day
-    new Date('2025-12-25').toLocaleString("en-US", { timeZone: "America/New_York" }), // Christmas Day
+    new Date(2025, 0, 1), // New Year's Day
+    new Date(2025, 0, 16), // Martin Luther King Jr. Day
+    new Date(2025, 1, 17), // Presidents Day
+    new Date(2025, 2, 17), // St. Patrick's Day
+    new Date(2025, 3, 18), // Good Friday
+    new Date(2025, 4, 26), // Memorial Day
+    new Date(2025, 6, 4), // Independence Day
+    new Date(2025, 8, 1), // Labor Day
+    new Date(2025, 9, 13), // Columbus Day
+    new Date(2025, 10, 11), // Veterans Day
+    new Date(2025, 11, 27), // Thanksgiving Day
+    new Date(2025, 11, 25), // Christmas Day
   ];
 
 
 // Function to check if a date is a bank holiday
 export function bankHoliday(date: Date): boolean {
   const dateString = date.toLocaleString("en-US", { timeZone: "America/New_York" });
-  const isBankHoliday = usBankHolidays.some((holiday: string) => 
-    holiday === dateString
+  const isBankHoliday = usBankHolidays.some((holiday: Date) => 
+    holiday.toLocaleString("en-US", { timeZone: "America/New_York" }) === dateString
   );
   return isBankHoliday;
 }
